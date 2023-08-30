f18336447268746df1a9f53f530464a18d47f823
liqsuq <49895092+liqsuq@users.noreply.github.com>
2023-08-30 14:11:27 +0900

    Update environment.bat

--- environment.bat
+++ environment.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set DIR=%~dp0
-set PYTHON=Python310
+set PYTHON=Python
 set PYTHONPATH=%DIR%\%PYTHON%
 set PATH=C:\Windows\system32;C:\Windows;%DIR%\PortableGit\bin;%PYTHONPATH%;%PYTHONPATH%\Scripts
